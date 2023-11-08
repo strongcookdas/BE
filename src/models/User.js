5b04f8ce693cbd0f13abf91ea6a8426b84509377
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-08 17:50:29 +0900

    feat: User 스키마 정의

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	nickname: { type: String, required: true, unique: true },
+	profile: { type: String },
+});
+
+const User = mongoose.model("User", userSchema);
